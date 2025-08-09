8b53300223e2cd517e7301ab11a282fddc1553b6
Crush-on-Study <twonkang00@naver.com>
2025-08-09 17:09:48 +0900

    [fix] modal popup contents update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Header from '../components/common/Header';
 import Orb from '../components/external/orb';
 import Button from '../components/common/button';
@@ -29,14 +29,15 @@ const HomePage = () => {
     setShowPopup(false);
   };
 
-  const letter = [
+  const letter = useMemo(() => ([
     '반갑습니다.',
     '저는 제가 무엇을 좋아하는지 깨닫기 위해 다양한 경험, 다양한 포지션을 경험해왔습니다.',
     '누구는 처음부터 원하던 전공, 원하던 직무를 찾았지만  저는 그렇지 못했습니다.',
     '그렇게 대략 4년간 하고 싶은게 무엇인지 찾아다니다보니 제일 재밌었던건 프론트엔드 개발자였습니다.',
     '저의 상상을 빈 화면에서 시작하여 채워나가는게 재밌습니다.',
-    '이 공간은 제가 그동안 경험했던 것들을 기록해놓았습니다.'
-  ];
+    '이 공간은 제가 그동안 경험했던 것들을 기록해놓았습니다.',
+    '제 작은 세상에 와주셔서 감사합니다.'
+  ]), []);
 
   return (
     <div className="home-page">
